0f4738db2f3683dbc85931629af19a9f1f024709
Tyler Sturos <tyler.john@qcommander.sh>
2024-09-04 17:35:12 -0800

    link logo to basecolors.com

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,20 @@
 'use client';
 import React from 'react';
 import Image from 'next/image';
-import { useAccount, useConnect, useDisconnect } from 'wagmi';
+import { useAccount } from 'wagmi';
 import WalletWrapper from './WalletWrapper';
 import LoginButton from './LoginButton';
-import SignupButton from './SignupButton';
 
 export default function Header() {
-  const { address, isConnected } = useAccount();
-  const { connect, connectors } = useConnect();
-  const { disconnect } = useDisconnect();
-
-  const handleConnect = async (connector: any) => {
-    await connect({ connector });
-  };
-
-  const handleDisconnect = () => {
-    disconnect();
-  };
+  const { isConnected } = useAccount();
 
   return (
     <header className="w-full bg-gray-100 py-4 pl-0 pr-3">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         <div className="flex items-center gap-4 mr-3">
-          <Image src="/base-colors-3dhc.png" alt="Base Colors 3DHC Logo" width={210} height={70} />
+          <a href="https://basecolors.com">
+            <Image src="/base-colors-3dhc.png" alt="Base Colors 3DHC Logo" width={210} height={70} />
+          </a>
         </div>
         <div className="flex items-center gap-3">
           {isConnected ? (
